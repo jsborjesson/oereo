e43c22dcabc07791ec33f6b036eaeb5e6bf43416
alcesleo <lagginglion@gmail.com>
2014-01-30 17:50:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# oereo
+
+...is palindrome made from the abbreviation **OER** _(**O**pen **E**ducational **R**esource)_
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oereo::Application.config.secret_key_base = 'e1d5ac02cefa03fc2b06b2a391addc9f4f29dea1ef0e38b236cdeac4f496c32240977fcdb52b3efdf6f7c75096d4ef67d8f07b565d42de47b7628a480782f7a5'
