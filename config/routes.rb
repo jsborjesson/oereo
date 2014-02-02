3d95403bcaa5c5e7e4c8e30fde903c3b2f917360
alcesleo <lagginglion@gmail.com>
2014-02-03 00:00:53 +0100

    Adding welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Oereo::Application.routes.draw do
+
+  resources :resources
+
+  root to: 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
