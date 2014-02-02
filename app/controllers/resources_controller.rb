a6d5047233b6382a43de5aa049b4f9288ec6f07c
alcesleo <lagginglion@gmail.com>
2014-02-03 00:14:50 +0100

    Supporting simple json response for resources

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,2 +1,13 @@
 class ResourcesController < ApplicationController
+  def index
+    @resources = Resource.all
+  end
+
+  def show
+    @resource = Resource.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @resource }
+    end
+  end
 end
