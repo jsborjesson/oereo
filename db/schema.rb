a00e9a15963b0ba800e505fc0fadbb348e8d3227
alcesleo <lagginglion@gmail.com>
2014-02-03 00:31:11 +0100

    Created api_key model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140202224254) do
+ActiveRecord::Schema.define(version: 20140202232628) do
 
   create_table "api_keys", force: true do |t|
     t.string   "access_token"
