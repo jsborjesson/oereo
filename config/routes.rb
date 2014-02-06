bac0bf74abb447c7c47b3ccdda02ad4b45a86ee9
alcesleo <lagginglion@gmail.com>
2014-02-06 17:57:28 +0100

    Log out and links on home screen

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ require 'api_constraints'
 Oereo::Application.routes.draw do
 
   get 'log_in' => 'sessions#new', as: 'log_in'
+  get 'log_out' => 'sessions#destroy', as: 'log_out'
   resources :sessions
   get 'sign_up' => 'users#new', as: 'sign_up'
   root to: 'users#new'
