aeff0ddd625566366f67e5e67b33d5048461f8ba
alcesleo <lagginglion@gmail.com>
2014-02-07 17:47:17 +0100

    Relate resources and tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140207163750) do
+ActiveRecord::Schema.define(version: 20140207164612) do
 
   create_table "api_keys", force: true do |t|
     t.string   "access_token"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20140207163750) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "url"
+  end
+
+  create_table "resources_tags", force: true do |t|
+    t.integer "resource_id"
+    t.integer "tag_id"
   end
 
   create_table "tags", force: true do |t|
