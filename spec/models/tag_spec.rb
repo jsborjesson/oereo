d7f085a7ce186ee7c2cb8a9cc0de6ea79fce2b95
alcesleo <lagginglion@gmail.com>
2014-02-07 18:43:49 +0100

    Adding and passing some tests for tag

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Tag do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "cannot be empty" do
+    empty_tag = build(:tag, name: '')
+    expect(empty_tag).not_to be_valid
+  end
+
+  it "should lower the case of inputted text"
+
+  it "should have a unique name" do
+    create(:tag, name: 'duplicate')
+    duplicate = build(:tag, name: 'duplicate')
+    expect(duplicate).not_to be_valid
+  end
+
 end
