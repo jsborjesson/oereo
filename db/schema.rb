ea3683a030e22bf5a6741ad9ccd84f5d006f8e87
alcesleo <lagginglion@gmail.com>
2014-02-07 17:38:11 +0100

    Create tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140206170658) do
+ActiveRecord::Schema.define(version: 20140207163750) do
 
   create_table "api_keys", force: true do |t|
     t.string   "access_token"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20140206170658) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "url"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
