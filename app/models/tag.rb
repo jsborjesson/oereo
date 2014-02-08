2492db55b62b2c182fbbe938f9b279c42ad0d84a
alcesleo <lagginglion@gmail.com>
2014-02-08 15:00:29 +0100

    Lower case tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,12 @@ class Tag < ActiveRecord::Base
 
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  before_validation :lower_name
+
+  protected
+
+  def lower_name
+    self.name.downcase!
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -7,7 +7,10 @@ describe Tag do
     expect(empty_tag).not_to be_valid
   end
 
-  it "should lower the case of inputted text"
+  it "should lower the case of inputted text" do
+    tag = create(:tag, name: 'TestTag')
+    expect(tag.name).to eq('testtag')
+  end
 
   it "should have a unique name" do
     create(:tag, name: 'duplicate')
