590cc7de49aac226453b4f1cd747993cefdde8d0
alcesleo <lagginglion@gmail.com>
2014-02-10 14:45:42 +0100

    Comments

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,7 @@
 class Resource < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
+  # Prefixes http:// if not present
   def self.format_url(url)
     if url.to_s !~ url_regexp && "http://#{url}" =~ url_regexp
       "http://#{url}"
@@ -9,6 +10,7 @@ class Resource < ActiveRecord::Base
     end
   end
 
+  # Matches a url, stolen from railscasts #301
   def self.url_regexp
     /\Ahttps?:\/\/([^\s:@]+:[^\s:@]*@)?[-[[:alnum:]]]+(\.[-[[:alnum:]]]+)+\.?(:\d{1,5})?([\/?]\S*)?\z/iux
   end
