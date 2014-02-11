291352284f2c18ec1297034e5c7712cbd11201e8
alcesleo <lagginglion@gmail.com>
2014-02-11 17:33:30 +0100

    Added a few licenses to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,12 @@
 
 User.create(email: 'test@email.com', password: 'test', password_confirmation: 'test')
 
-Resource.create(url: 'www.google.com')
-Resource.create(url: 'www.lmgtfy.com')
-Resource.create(url: 'www.stackoverflow.com')
-Resource.create(url: 'www.wikipedia.org')
+Resource.create!(url: 'www.google.com')
+Resource.create!(url: 'www.lmgtfy.com')
+Resource.create!(url: 'www.stackoverflow.com')
+Resource.create!(url: 'www.wikipedia.org')
+
+License.create!(title: 'MIT License', agreement_url: 'http://opensource.org/licenses/MIT')
+License.create!(title: 'GPL v2', agreement_url: 'http://www.gnu.org/licenses/gpl-2.0.html')
+License.create!(title: 'Apache License v2', agreement_url: 'http://www.apache.org/licenses/LICENSE-2.0.html')
+License.create!(title: 'Public Domain (no license)', agreement_url: 'http://en.wikipedia.org/wiki/Public_domain')
