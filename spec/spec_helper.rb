6f83949373bee9a8ce2c8eb087ba0d5f89de8a0f
alcesleo <lagginglion@gmail.com>
2014-02-11 20:29:55 +0100

    Clean up rspec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,14 +13,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  # spec_helper.rb
 
   # Disable old should-syntax
   config.expect_with :rspec do |c|
@@ -30,22 +22,13 @@ RSpec.configure do |config|
   # Enable shorter syntax for factory girl
   config.include FactoryGirl::Syntax::Methods
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = false
-
+  # Configure database_cleaner
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
   end
-
   config.before(:each) do
     DatabaseCleaner.start
   end
-
   config.after(:each) do
     DatabaseCleaner.clean
   end
