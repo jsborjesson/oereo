72554139136a20295cca56fa3354876cea68a2cf
alcesleo <lagginglion@gmail.com>
2014-02-12 12:40:32 +0100

    Fix missing fk in resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212092910) do
+ActiveRecord::Schema.define(version: 20140212113916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20140212092910) do
     t.string   "url"
     t.string   "title"
     t.text     "description"
+    t.integer  "user_id"
   end
 
   create_table "resources_tags", force: true do |t|
