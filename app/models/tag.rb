d8539484d7b632f2824438a2063a0e46b8694149
alcesleo <lagginglion@gmail.com>
2014-02-14 15:33:00 +0100

    Renamed to tag name

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,16 +1,16 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :resources
 
-  validates_presence_of :name
-  validates_uniqueness_of :name
+  validates_presence_of :tag_name
+  validates_uniqueness_of :tag_name
 
   before_validation :lower_name
 
   protected
 
   def lower_name
-    unless self.name.nil?
-      self.name.downcase!
+    unless self.tag_name.nil?
+      self.tag_name.downcase!
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213133401) do
+ActiveRecord::Schema.define(version: 20140214143216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 20140213133401) do
   end
 
   create_table "tags", force: true do |t|
-    t.string   "name"
+    t.string   "tag_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
