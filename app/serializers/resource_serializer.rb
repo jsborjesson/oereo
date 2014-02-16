4f9d2d69151a243b5b8c61c3b84e32a6ca681f31
alcesleo <lagginglion@gmail.com>
2014-02-16 23:01:29 +0100

    Much cleaner tags in resources

--- app/serializers/resource_serializer.rb
+++ app/serializers/resource_serializer.rb
@@ -1,7 +1,9 @@
 class ResourceSerializer < ActiveModel::Serializer
   attributes :title, :url, :description, :links, :tags
 
-  has_many :tags
+  def tags
+    @object.tags.pluck(:tag_name)
+  end
 
   def links
     # FIXME: Bug in the fork of AMS prevents access to url-methods
