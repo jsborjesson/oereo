043458b5a1bb4f0e7bfe6f51be7beb31a5651420
alcesleo <lagginglion@gmail.com>
2014-03-05 13:07:16 +0100

    Listing a couple of dummy url:s in a ul

---
+++ app/assets/javascripts/angular/controllers.js
@@ -0,0 +1,7 @@
+function HomeController($scope) {
+    $scope.resources = [
+        { url: 'www.google.com' },
+        { url: 'www.stackoverflow.com' },
+        { url: 'www.railscasts.com' }
+    ];
+}
