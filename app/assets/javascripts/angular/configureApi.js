50368589594ce90ada145f929dd45ade2eec7117
alcesleo <lagginglion@gmail.com>
2014-03-18 16:47:54 +0100

    Take care of meta when received

--- app/assets/javascripts/angular/configureApi.js
+++ app/assets/javascripts/angular/configureApi.js
@@ -7,19 +7,21 @@ oereoApp.run(function (Restangular, parsePaginationHeader) {
 
   // Parse the response
   Restangular.addResponseInterceptor(function(data, operation, what, url, response) {
+    var extracted;
     console.log('Received: ', data);
 
     if (operation === 'getList') {
-      data = data[what];
-      data.pagination = parsePaginationHeader(response.headers('Link'));
-      console.log('Extracted list: ', data);
+      extracted = data[what];
+      extracted.links = parsePaginationHeader(response.headers('Link'));
+      extracted.meta = data['meta'];
+      console.log('Extracted list: ', extracted);
     }
     else if (operation === 'get') {
-      data = data[what.slice(0, -1)]; // Remove the S
-      console.log('Extracted object: ', data);
+      extracted = data[what.slice(0, -1)]; // Remove the S
+      console.log('Extracted object: ', extracted);
     }
 
-    return data;
+    return extracted;
   });
 
 });
