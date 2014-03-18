44391c3f0c8758f7e88a0b239c2f9956ec46a003
alcesleo <lagginglion@gmail.com>
2014-03-18 15:08:12 +0100

    Removed fixed todo

--- app/assets/javascripts/angular/factories.js
+++ app/assets/javascripts/angular/factories.js
@@ -1,7 +1,6 @@
 angular.module('factories', [])
 .factory('parsePaginationHeader', function () {
   return function (header) {
-    // TODO: Break this out, maybe a service?
     // Turns a Link header with pagination into an object, parses things like this:
     // <http://localhost:3000/api/resources?page=2>; rel="next", <http://localhost:3000/api/resources?page=3>; rel="last"
     var links = {},
