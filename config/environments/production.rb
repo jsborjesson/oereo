de7134b5c39f3abb4667e41204267991c6782af9
Jimmy Börjesson <lagginglion@gmail.com>
2014-04-24 10:33:47 +0200

    Commented on js compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Oereo::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
-  # Compress JavaScripts and CSS.
+  # Compress JavaScripts without messing up Angulars dependency injection
   config.assets.js_compressor = Uglifier.new(:mangle => false)
   # config.assets.css_compressor = :sass
 
